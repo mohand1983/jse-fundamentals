6d25b524b424d86c39d4d10ddc838f308e512373
boudi1983 <boudi.mohand@gmail.com>
2023-02-27 11:50:20 +0100

    Add a MediaListDto and MediaDetailDto

---
+++ src/main/java/fr/aelion/helpers/dto/MediaDetailDto.java
@@ -0,0 +1,35 @@
+package fr.aelion.helpers.dto;
+
+import fr.aelion.helpers.MediaBuilder;
+import fr.aelion.helpers.interfaces.Builder;
+import fr.aelion.models.course.Media;
+
+public class MediaDetailDto {
+    /**
+     * title of a madia
+     */
+    public String title;
+    /**
+     * Summary of a media
+     */
+    public String summary;
+    /**
+     * Duration of a media
+     */
+    public Float duration;
+    /**
+     * mediaType of a media
+     */
+    public String mediaType;
+
+    /**
+     *
+     * @param media
+     */
+    public void deserialize(Media media){
+        this.title=media.getTitle();
+        this.duration= media.getDuration();
+        this.summary= media.getSummary();
+        this.mediaType=media.getClass().getSimpleName().substring(0,1);
+    }
+}
---
+++ src/main/java/fr/aelion/helpers/dto/MediaListDto.java
@@ -0,0 +1,25 @@
+package fr.aelion.helpers.dto;
+
+import fr.aelion.models.course.Media;
+
+public class MediaListDto {
+    /**
+     * title of a media
+     * @property String title
+     */
+    public String title;
+    /**
+     * duration of a media
+     */
+    public Float duration;
+
+    /**
+     * transform a media object to a mediaListDto object
+     * @param media media to deserialize to mediaListDto
+     */
+    public void deserialize(Media media){
+        this.title=media.getTitle();
+        this.duration= media.getDuration();
+    }
+
+}
