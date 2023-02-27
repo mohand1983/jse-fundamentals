ff9384e605a5dc2e5f008fec786b95ce03f7dbc8
boudi1983 <boudi.mohand@gmail.com>
2023-02-27 16:05:51 +0100

    Add itial annotation

--- src/main/java/fr/aelion/helpers/dto/MediaDetailDto.java
+++ src/main/java/fr/aelion/helpers/dto/MediaDetailDto.java
@@ -1,10 +1,12 @@
 package fr.aelion.helpers.dto;
 
 import fr.aelion.helpers.MediaBuilder;
+import fr.aelion.helpers.dto.annotations.Initial;
 import fr.aelion.helpers.interfaces.Builder;
 import fr.aelion.models.course.Media;
 
 public class MediaDetailDto {
+
     /**
      * title of a madia
      */
@@ -20,16 +22,17 @@ public class MediaDetailDto {
     /**
      * mediaType of a media
      */
+    @Initial
     public String mediaType;
 
     /**
      *
      * @param media
      */
-    public void deserialize(Media media){
+    /*public void deserialize(Media media){
         this.title=media.getTitle();
         this.duration= media.getDuration();
         this.summary= media.getSummary();
         this.mediaType=media.getClass().getSimpleName().substring(0,1);
-    }
+    }*/
 }
